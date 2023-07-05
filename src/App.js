2c1019f92ebec2b09ed6c135c06be7a1be5e401d
Rahul Jha <rahujha04@gmail.com>
2023-07-05 16:03:45 +0530

    frontend added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Auth } from "./Pages/Auth";
+import { CreateRecipe } from "./Pages/CreateRecipe";
+import { SavedRecipes } from "./Pages/SavedRecipes";
+import { Navbar } from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
